d04efaa432df82f389b3cbfa0229e1a247bbd879
Disspear574 <disspear574@mail.ru>
2023-04-28 11:50:28 +0500

    modified:   js/button.js 	modified:   portfolio.html   modified:   styles/style.css

--- js/button.js
+++ js/button.js
@@ -1,3 +1,14 @@
+// Swiper slider
+const swiper = new Swiper('.swiper', {
+	speed: 400,
+	spaceBetween: 100,
+	navigation: {
+		nextE1: '.swiper-button-next',
+		prevE1: '.swiper-button-prev',
+	},
+	effect: 'cube',
+})
+
 let btn = document.getElementById('btnup') // элемент с id - btnup становится переменной btn
 window.onscroll = function () {
 	scroll()
