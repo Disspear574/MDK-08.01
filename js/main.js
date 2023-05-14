bc25df578e521e8f62f8ef02720c5010162cd2b3
Disspear574 <disspear574@mail.ru>
2023-05-14 19:42:23 +0500

    modified:   booking.html 	new file:   js/ap.js   modified:   js/main.js 	modified:   templates/menu.html

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
-$(function () {
-	$('#menu .nav .menu a').each(function () {
-		var location = window.location.href
-		var link = this.href
-		if (location == link) {
-			$(this).addClass('active')
-		}
-	})
-})
-const activepage = window.location
-console.log(activepage)
 setTimeout(function () {
 	var element = document.getElementById('html')
 	element.classList.add('html_visible')
-}, 200)
+}, 100)
 
 // Swiper slider
 const swiper = new Swiper('.swiper', {
